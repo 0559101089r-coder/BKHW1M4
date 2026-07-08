16655dd4e2c0e9bf375c09bc32d330bfbd300311
0559101089r-coder <0559101089r@gmail.com>
2026-07-08 20:47:08 +0600

    3hw

--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import apps.nurai.models as Student
 
 # Create your views here.
+
+def index(request):
+    students = Student.Student.objects.all()
+    return render(request, 'index.html', locals())
